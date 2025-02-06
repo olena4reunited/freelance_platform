e2e798f380819825ff1be7fce7b234c237b338e7
olena4reunited <olenaduringintermission@gmail.com>
2025-02-06 11:57:30 +0200

    update user data validation on create and update; add method enum to variate data validation processing for different methods

--- server/app/routers/user_routers.py
+++ server/app/routers/user_routers.py
@@ -5,7 +5,7 @@ from server.app.schemas.users_schemas import (
     UserBase
 )
 from server.app.controllers.user_controller import UserController
-from server.app.validators.user_validators import validate_email, validate_password, validate_phone_number
+from server.app.validators.user_validators import UserValidator, MethodEnum
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -13,9 +13,15 @@ router = APIRouter(prefix="/users", tags=["users"])
 @router.post("/signup", response_model=UserResponse)
 def create_user(user_data: UserBase):
     try:
-        validate_email(user_data.email)
-        validate_password(user_data.password)
-        validate_phone_number(user_data.phone_number)
+        UserValidator(
+            MethodEnum.create,
+            user_data.email,
+            user_data.password,
+            user_data.phone_number) \
+            .validate_email() \
+            .validate_password() \
+            .validate_phone_number()
+
         return UserController.create_user(**user_data.model_dump())
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
@@ -32,12 +38,14 @@ def get_user(user_id: int):
 @router.patch("/{user_id}", response_model=UserResponse)
 def update_user(user_id: int, updated_user_data: dict):
     try:
-        if updated_user_data.get("email"):
-            validate_email(updated_user_data.get("email"))
-        if updated_user_data.get("password"):
-            validate_password(updated_user_data.get("password"))
-        if updated_user_data.get("phone_number"):
-            validate_phone_number(updated_user_data.get("phone_number"))
+        UserValidator(
+            MethodEnum.update,
+            updated_user_data.get("email", None),
+            updated_user_data.get("password", None),
+            updated_user_data.get("phone_number", None)) \
+            .validate_email() \
+            .validate_password() \
+            .validate_phone_number()
 
         return UserController.update_user(user_id, **updated_user_data)
     except Exception as e:
--- server/app/validators/user_validators.py
+++ server/app/validators/user_validators.py
@@ -1,28 +1,69 @@
 import re
+from enum import Enum
+
 from fastapi import HTTPException
 
 
-def validate_email(email: str):
-    email_regex = r"\w+@[a-zA-Z_]+\.[a-zA-Z]{2,6}"
-
-    if not re.match(email_regex, email):
-        raise HTTPException(status_code=400, detail="Invalid email")
+class MethodEnum(Enum):
+    create = 1
+    update = 0
 
 
-def validate_password(password):
-    if len(password) < 8:
-        raise HTTPException(status_code=400, detail="Password must be at least 8 characters long")
-    if not re.search(r"[A-Z]", password):
-        raise HTTPException(status_code=400, detail="Password must contain at least one uppercase letter")
-    if not re.search(r"[a-z]", password):
-        raise HTTPException(status_code=400, detail="Password must contain at least one lowercase letter")
-    if not re.search(r"\d", password):
-        raise HTTPException(status_code=400, detail="Password must contain at least one digit")
-    if not re.search(r"[!-\/:-@[-`{-~]", password):
-        raise HTTPException(status_code=400, detail="Password must contain at least one special character")
+class UserValidator:
+    def __init__(
+            self,
+            method: MethodEnum,
+            email: str | None = None,
+            password: str | None = None,
+            phone_number: str | None = None
+    ):
+        self.method = method.value
+        self.email = email
+        self.password = password
+        self.phone_number = phone_number
 
 
-def validate_phone_number(username):
-    phone_number_regex = r"^\+?\d{10, 12}$"
-    if not re.match(phone_number_regex, username):
-        raise HTTPException(status_code=400, detail="Invalid phone number")
+    def validate_email(self):
+        if not self.email and self.method:
+            raise HTTPException(status_code=400, detail="Email is required")
+        if not self.email and not self.method:
+            return self
+
+        email_regex = r"\w+@[a-zA-Z_]+\.[a-zA-Z]{2,6}"
+
+        if not re.match(email_regex, self.email):
+            raise HTTPException(status_code=400, detail="Invalid email")
+
+        return self
+
+
+    def validate_password(self):
+        if not self.password and self.method:
+            raise HTTPException(status_code=400, detail="Password is required")
+        if not self.password and not self.method:
+            return self
+
+        if len(self.password) < 8:
+            raise HTTPException(status_code=400, detail="Password must be at least 8 characters long")
+        if not re.search(r"[A-Z]", self.password):
+            raise HTTPException(status_code=400, detail="Password must contain at least one uppercase letter")
+        if not re.search(r"[a-z]", self.password):
+            raise HTTPException(status_code=400, detail="Password must contain at least one lowercase letter")
+        if not re.search(r"\d", self.password):
+            raise HTTPException(status_code=400, detail="Password must contain at least one digit")
+        if not re.search(r"[!-/:-@[-`{-~]", self.password):
+            raise HTTPException(status_code=400, detail="Password must contain at least one special character")
+
+        return self
+
+
+    def validate_phone_number(self):
+        if not self.phone_number:
+            return self
+
+        phone_number_regex = r"^\+?\d{10, 12}$"
+
+        if not re.match(phone_number_regex, self.phone_number):
+            raise HTTPException(status_code=400, detail="Invalid phone number")
+
+        return self
