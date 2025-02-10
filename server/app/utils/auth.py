f43d6974b4df5597415f8318c880cb25f065f578
olena4reunited <olenaduringintermission@gmail.com>
2025-02-10 13:54:34 +0200

    add access and refresh jwt token creation

--- server/app/utils/auth.py
+++ server/app/utils/auth.py
@@ -1,5 +1,6 @@
 import os
 from datetime import timedelta, datetime, timezone
+from typing import Any
 
 import jwt
 from cryptography.hazmat.primitives import serialization
@@ -7,6 +8,11 @@ from fastapi import HTTPException
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
 from starlette import status
+
+from server.app.utils.config import (
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    REFRESH_TOKEN_EXPIRE_DAYS
+)
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -32,21 +38,39 @@ with open(os.path.join(os.path.dirname(__file__), "../../keys/public.pem"), "rb"
     public_key = serialization.load_pem_public_key(key_file.read())
 
 
-def create_access_token(username: str):
-    exp = datetime.now(timezone.utc) + timedelta(minutes=30)
+def create_token(user_data: dict[str, Any], expires_in: timedelta):
+    exp = datetime.now(timezone.utc) + expires_in
 
     payload = {
-        "sub": username,
+        "sub": user_data["username"],
         "iat": datetime.now(timezone.utc).timestamp(),
-        "exp": exp
+        "exp": exp,
+        "content": user_data
     }
 
-    token = jwt.encode(payload=payload, key=private_key, alg="RS256")
+    token = jwt.encode(payload=payload, key=private_key, algorithm="RS256")
 
     return token
 
 
-def verify_token(token: str) -> dict[str, str] | None:
+def refresh_token(token: str):
+    payload = jwt.decode(token, key=public_key, algorithms=["RS256"])
+
+    user_data = payload["content"]
+
+    access_tkn = create_token(user_data, timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    refresh_tkn = create_token(user_data, timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS))
+
+    return (
+        {
+            "access_token": access_tkn,
+            "refresh_token": refresh_tkn,
+            "token_type": "bearer"
+        }
+    )
+
+
+def verify_token(token: str) -> dict[str, Any] | None:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -63,5 +87,3 @@ def verify_token(token: str) -> dict[str, str] | None:
 
     except jwt.PyJWTError:
         raise credentials_exception
-
-
