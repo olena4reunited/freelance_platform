565269c8670c6e2b0a5c9e8be2372fad6c5318f1
olena4reunited <olenaduringintermission@gmail.com>
2025-02-11 16:24:42 +0200

    build get_permission for permissions table

--- server/app/models/models.py
+++ server/app/models/models.py
@@ -11,6 +11,23 @@ class Plan(BaseModel):
 class Permission(BaseModel):
     table_name = "permissions"
 
+    @staticmethod
+    def get_permissions(plan_name: str | None = None) -> list[dict[str, str]] | dict[str, str]:
+        query = """
+            SELECT pln.name as plan, prm.name as permission
+            FROM permissions prm
+            LEFT JOIN plans_permissions pp ON prm.id = pp.permission_id
+            LEFT JOIN plans pln ON pp.plan_id = pln.id
+        """
+        params = ()
+
+        if plan_name:
+            query += " WHERE pln.name = %s"
+            params = (plan_name,)
+
+        with PostgresDatabase() as db:
+            return db.fetch(query, params, is_all=True)
+
 
 class PlanPermission(BaseModel):
     table_name = "plans_permissions"
