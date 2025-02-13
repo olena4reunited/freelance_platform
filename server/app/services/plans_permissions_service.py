f155b8993c7e5e0da1ebd4ac1f7b145b261f0c3f
olena4reunited <olenaduringintermission@gmail.com>
2025-02-13 18:56:39 +0200

    start for service to fill dtables plans permissions and connections between them

---
+++ server/app/services/plans_permissions_service.py
@@ -0,0 +1,48 @@
+from server.app.database.database import PostgresDatabase
+
+
+def set_plans_permissions():
+    with PostgresDatabase(on_commit=True) as db:
+        db.execute_query(
+            """
+            INSERT INTO plans (name)
+            VALUES
+                ('admin'),
+                ('moderator'),
+                ('customer'),
+                ('performer');
+            """
+        )
+
+        db.execute_query(
+            """
+                CREATE OR REPLACE FUNCTION insert_permissions(json_data jsonb)
+                RETURNS VOID AS $$
+                DECLARE
+                    permission_record RECORD;
+                    perm_id INT;
+                    plan_name TEXT;
+                BEGIN
+                    FOR permission_record IN
+                        SELECT
+                            p ->> 'name' AS name,
+                            p -> 'plans' AS plans
+                        FROM jsonb_array_elements(json_data) p
+                    LOOP
+                        INSERT INTO permissions (name)
+                        VALUES (permission_record.name)
+                        RETURNING id INTO perm_id;
+                
+                        FOR plan_name IN
+                            SELECT jsonb_array_elements_text(permission_record.plans)
+                        LOOP
+                            INSERT INTO plans_permissions (plan_id, permission_id)
+                            SELECT id, perm_id
+                            FROM plans
+                            WHERE name = plan_name;
+                        END LOOP;
+                    END LOOP;
+                END;
+                $$ LANGUAGE plpgsql;
+            """
+        )
