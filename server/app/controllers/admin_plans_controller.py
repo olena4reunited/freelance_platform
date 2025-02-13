fdd5f9d9c4f4312250ad11cfb47ff62ffebc719b
olena4reunited <olenaduringintermission@gmail.com>
2025-02-13 17:09:21 +0200

    create admin plans controllers

--- server/app/controllers/admin_plans_controller.py
+++ server/app/controllers/admin_plans_controller.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+from server.app.database.database import PostgresDatabase
 from server.app.models.plan_model import Plan
 
 
@@ -9,5 +10,20 @@ class AdminPlansController:
         return Plan.get_all_records()
 
     @staticmethod
-    def get_plan_by_id(plan_id: int) -> dict[str, Any] | None:
+    def create_plan(plan_data: dict[str, str]) -> dict[str, Any]:
+        return Plan.create_record(**plan_data)
+
+    @staticmethod
+    def get_plan_by_id(plan_id: int) -> dict[str, Any]:
         return Plan.get_plan_detail_by_id(plan_id)
+
+    @staticmethod
+    def update_plan_by_id(
+            plan_id: int,
+            plan_data: dict[str, str]
+    ) -> dict[str, Any]:
+        return Plan.update_plan_by_id(plan_id, plan_data["plan"])
+
+    @staticmethod
+    def delete_plan_by_id(plan_id: int) -> None:
+        Plan.delete_record_by_id(plan_id)
