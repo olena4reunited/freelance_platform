6be88aad4b1b85966611a72dfee13b938e006bee
olena4reunited <olenaduringintermission@gmail.com>
2025-02-13 12:02:09 +0200

    create payment controller

--- server/app/controllers/payment_controller.py
+++ server/app/controllers/payment_controller.py
@@ -2,13 +2,13 @@ from typing import Any
 
 from server.app.database.database import PostgresDatabase
 from server.app.models.models import Payment
-from server.app.utils.crypto import encrypt_data, decrypt_data, get_masked_payment
+from server.app.utils.crypto import encrypt_data, get_masked_payment
 
 
 class PaymentController:
     @staticmethod
     def create_payment(user_id: int, payment: str) -> dict[str, str]:
-        encrypted_payment = encrypt_data(bytes(payment, "unf-8"))
+        encrypted_payment = encrypt_data(bytes(payment, "utf-8"))
         payment = Payment.create_record(user_id=user_id, payment=encrypted_payment)
         payment["payment"] = get_masked_payment(payment["payment"])
         payment.pop("user_id")
@@ -24,7 +24,7 @@ class PaymentController:
         return payment
 
     @staticmethod
-    def get_all_user_payments(user_id: int) -> list[dict[str, str]]:
+    def get_user_payments(user_id: int) -> list[dict[str, str]]:
         payments = Payment.get_payments_by_user(user_id)
 
         for payment in payments:
@@ -36,3 +36,21 @@ class PaymentController:
     def delete_payment(payment_id: int) -> None:
         Payment.delete_record_by_id(payment_id)
 
+    @staticmethod
+    def get_all_users_payments():
+        payments = Payment.get_all_records()
+
+        for payment in payments:
+            payment["payment"] = get_masked_payment(payment["payment"])
+
+        return payments
+
+    @staticmethod
+    def get_user_payment_details(user_id: int, payment_id: int) -> dict[str, Any] | None:
+        with PostgresDatabase() as db:
+            payment = db.fetch(
+                f"SELECT id, user_id, payment FROM payments WHERE user_id = %s AND id = %s",
+                (user_id, payment_id),
+            )
+
+        payment["payment"] = get_masked_payment(payment["payment"])
