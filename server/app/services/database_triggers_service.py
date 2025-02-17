5d339de36e45ed27e7fe2a201d8645e4479760c6
olena4reunited <olenaduringintermission@gmail.com>
2025-02-17 15:25:39 +0200

    create db triggers for deleting blocked records and users

---
+++ server/app/services/database_triggers_service.py
@@ -0,0 +1,52 @@
+from server.app.database.database import PostgresDatabase
+
+
+def create_trigger_delete_blocked_records():
+    with PostgresDatabase() as db:
+        db.execute_query(
+            """
+                CREATE OR REPLACE FUNCTION delete_blocked_records()
+                    RETURNS TRIGGER AS $$
+                        BEGIN
+                            DELETE FROM orders
+                            WHERE blocked_until IS NULL AND is_blocked = TRUE;
+                    
+                            RETURN NULL;
+                        END;
+                    $$ LANGUAGE plpgsql;
+            """
+        )
+
+        db.execute_query(
+            """
+                CREATE TRIGGER trigger_delete_blocked_orders
+                AFTER UPDATE ON orders
+                FOR EACH STATEMENT
+                EXECUTE PROCEDURE delete_blocked_records();
+            """
+        )
+
+def create_trigger_delete_blocked_users():
+    with PostgresDatabase() as db:
+        db.execute_query(
+            """
+                CREATE OR REPLACE FUNCTION delete_blocked_users()
+                RETURNS TRIGGER AS $$
+                    BEGIN
+                        DELETE FROM users
+                        WHERE block_expired IS NULL AND is_blocked = TRUE;
+                
+                        RETURN NULL;
+                    END;
+                $$ LANGUAGE plpgsql;
+            """
+        )
+
+        db.execute_query(
+            """
+                CREATE TRIGGER trigger_delete_blocked_users
+                AFTER UPDATE ON users
+                FOR EACH STATEMENT
+                EXECUTE PROCEDURE delete_blocked_users();
+            """
+        )
