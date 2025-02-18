1b5511eb9189bbdf25a485a157e2ddae97a2151a
olena4reunited <olenaduringintermission@gmail.com>
2025-02-18 18:47:26 +0200

    orer schemas for descriptive view

--- server/app/schemas/order_schemas.py
+++ server/app/schemas/order_schemas.py
@@ -4,11 +4,45 @@ from pydantic import BaseModel
 class OrderCreate(BaseModel):
     name: str
     description: str
+    images_links: list[str] | None = None
+
+
+class OrderUpdate(OrderCreate):
+    pass
 
 
 class OrderResponse(BaseModel):
     id: int
     name: str
     description: str
-    creator_id: int
+    customer_id: int
     performer_id: int | None = None
+
+
+class OrderListResponse(BaseModel):
+    id: int
+    name: str
+    description: str
+    customer_id: int
+    performer_id: int | None = None
+    image_link: str | None
+
+
+class OrderSingleResponse(BaseModel):
+    id: int
+    name: str
+    description: str
+    customer_id: int
+    performer_id: int | None = None
+    images_links: list[str] | None
+
+
+class OrderListPerformerResponse(BaseModel):
+    id: int
+    name: str
+    description: str
+    customer_id: int
+
+
+class OrderPerformerAssignedResponse(OrderListPerformerResponse):
+    performer_id: int
