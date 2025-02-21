7d2f5dc8bd2aecb497f622951f3f32e849a151eb
olena4reunited <olenaduringintermission@gmail.com>
2025-02-21 17:20:40 +0200

    create triggers for setting blocked timestamp

--- server/app/services/database_triggers_service.py
+++ server/app/services/database_triggers_service.py
@@ -1,14 +1,14 @@
 from server.app.database.database import PostgresDatabase
 
 
-def create_trigger_delete_blocked_records():
-    with PostgresDatabase() as db:
+def create_routines():
+    with PostgresDatabase(on_commit=True) as db:
         db.execute_query(
             """
-                CREATE OR REPLACE FUNCTION delete_blocked_records()
+                CREATE OR REPLACE FUNCTION set_blocked_timestamp_orders()
                 RETURNS TRIGGER AS $$
                     BEGIN
-                        DELETE FROM orders
+                        UPDATE orders SET blocked_until = CURRENT_TIMESTAMP + INTERVAL '30 days'
                         WHERE blocked_until IS NULL AND is_blocked = TRUE;
                 
                         RETURN NULL;
@@ -19,35 +19,40 @@ def create_trigger_delete_blocked_records():
 
         db.execute_query(
             """
-                CREATE TRIGGER trigger_delete_blocked_orders
-                AFTER UPDATE ON orders
-                FOR EACH STATEMENT
-                EXECUTE PROCEDURE delete_blocked_records();
-            """
-        )
-
-
-def create_trigger_delete_blocked_users():
-    with PostgresDatabase() as db:
-        db.execute_query(
-            """
-                CREATE OR REPLACE FUNCTION delete_blocked_users()
+                CREATE OR REPLACE FUNCTION set_blocked_timestamp_users()
                 RETURNS TRIGGER AS $$
                     BEGIN
-                        DELETE FROM users
+                        UPDATE users SET block_expired = CURRENT_TIMESTAMP + INTERVAL '30 days' 
                         WHERE block_expired IS NULL AND is_blocked = TRUE;
-                
+
                         RETURN NULL;
                     END;
                 $$ LANGUAGE plpgsql;
             """
         )
 
+
+def create_triggers():
+    with PostgresDatabase(on_commit=True) as db:
         db.execute_query(
             """
-                CREATE TRIGGER trigger_delete_blocked_users
-                AFTER UPDATE ON users
+                CREATE TRIGGER trigger_set_blocked_timestamp_orders
+                AFTER UPDATE ON orders
                 FOR EACH STATEMENT
-                EXECUTE PROCEDURE delete_blocked_users();
+                EXECUTE PROCEDURE set_blocked_timestamp_orders();
             """
         )
+
+        db.execute_query(
+            """
+                CREATE TRIGGER trigger_set_blocked_timestamp_users
+                AFTER UPDATE ON users
+                FOR EACH STATEMENT
+                EXECUTE PROCEDURE set_blocked_timestamp_users();
+            """
+        )
+
+
+if __name__ == "__main__":
+    create_routines()
+    create_triggers()
