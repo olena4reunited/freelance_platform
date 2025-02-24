f2f9afc37516e1cd58feafcc31532b0a993bf93c
olena4reunited <olenaduringintermission@gmail.com>
2025-02-24 16:12:38 +0200

    feedbacks & feedbacks_images "models"

---
+++ server/app/models/profile_feedback_image_model.py
@@ -0,0 +1,5 @@
+from server.app.models._base_model import BaseModel
+
+
+class ProfileFeedbackImage(BaseModel):
+    table_name = "profile_feedbacks_images"
---
+++ server/app/models/users_profile_feedback_model.py
@@ -0,0 +1,5 @@
+from server.app.models._base_model import BaseModel
+
+
+class UserProfileFeedback(BaseModel):
+    table_name = "users_profile_feedbacks"
