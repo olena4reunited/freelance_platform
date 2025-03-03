8d36596cb4d532aa6f28eafde844f73e25cef7bf
olena4reunited <olenaduringintermission@gmail.com>
2025-03-03 11:35:51 +0200

    feat: create db trigger for updating user rating on new or updated review

--- server/app/services/database_triggers_service.py
+++ server/app/services/database_triggers_service.py
@@ -16,7 +16,6 @@ def create_routines():
                 $$ LANGUAGE plpgsql;
             """
         )
-
         db.execute_query(
             """
                 CREATE OR REPLACE FUNCTION set_blocked_timestamp_users()
@@ -30,25 +29,51 @@ def create_routines():
                 $$ LANGUAGE plpgsql;
             """
         )
+        db.execute_query(
+            """
+                CREATE OR REPLACE FUNCTION update_user_rating()
+                RETURNS TRIGGER AS $$
+                    BEGIN
+                        WITH aggregated_rating AS (
+                            SELECT avg(rate) AS avg_rating, profile_id
+                            FROM users_profile_feedbacks
+                            WHERE profile_id = NEW.profile_id
+                            GROUP BY profile_id
+                        )
+                        UPDATE users
+                        SET rating = aggr.avg_rating
+                        FROM aggregated_rating aggr
+                        WHERE id = aggr.profile_id;
+                    END;
+                $$ LANGUAGE plpgsql;
+            """
+        )
 
 
 def create_triggers():
     with PostgresDatabase(on_commit=True) as db:
         db.execute_query(
             """
-                CREATE TRIGGER trigger_set_blocked_timestamp_orders
+                CREATE OR REPLACE TRIGGER trigger_set_blocked_timestamp_orders
                 AFTER UPDATE ON orders
                 FOR EACH STATEMENT
                 EXECUTE PROCEDURE set_blocked_timestamp_orders();
             """
         )
-
         db.execute_query(
             """
-                CREATE TRIGGER trigger_set_blocked_timestamp_users
+                CREATE OR REPLACE TRIGGER trigger_set_blocked_timestamp_users
                 AFTER UPDATE ON users
                 FOR EACH STATEMENT
                 EXECUTE PROCEDURE set_blocked_timestamp_users();
+            """
+        )
+        db.execute_query(
+            """
+                CREATE OR REPLACE TRIGGER trigger_update_user_rating
+                AFTER INSERT OR UPDATE ON users_profile_feedbacks
+                FOR EACH STATEMENT
+                EXECUTE PROCEDURE update_user_rating();
             """
         )
 
