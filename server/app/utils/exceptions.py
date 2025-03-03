cdec330d2273a4691930e665a726bf6a0c210073
olena4reunited <olenaduringintermission@gmail.com>
2025-03-03 10:53:00 +0200

    upd: enhanced exception class with GlobalException inherited from Exception

--- server/app/utils/exceptions.py
+++ server/app/utils/exceptions.py
@@ -2,78 +2,77 @@ from typing import Callable, Any
 from functools import wraps
 
 from fastapi import HTTPException
-from fastapi.exceptions import ResponseValidationError
+from fastapi.exceptions import ResponseValidationError, ValidationException
 from psycopg2.errors import DatabaseError, OperationalError, IntegrityError
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 
-
-class CustomHTTPException(HTTPException):
-    def __init__(
-            self,
-            status_code: int,
-            detail: str | None = None,
-            extra: dict | None = None
-    ) -> None:
-        super().__init__(status_code=status_code, detail=detail)
-        self.extra = extra
-
-    def to_dict(self):
-        return {
-            "status_code": self.status_code,
-            "detail": self.detail,
-            "extra": self.extra,
-        }
-
-    @classmethod
-    def raise_exception(
-            cls,
-            status_code: int,
-            detail: str | None = None,
-            extra: dict | None = None
-    ) -> JSONResponse:
-        raise cls(status_code=status_code, detail=detail, extra=extra)
-
+class GlobalException(Exception):
     @classmethod
     def catcher(cls, func: Callable) -> Callable:
         @wraps(func)
-        def wrapper(*args: Any, **kwargs: Any) -> JSONResponse:
+        def wrapper(*args: Any, **kwargs: Any) -> Exception:
             try:
                 return func(*args, **kwargs)
             except IntegrityError as e:
-                cls.raise_exception(
+                GlobalException.CustomHTTPException.raise_exception(
                     500,
                     "Database integrity error",
-                    extra={"pgerror": str(e)}
+                    extra={"error": str(e)}
                 )
             except (DatabaseError, OperationalError) as e:
-                cls.raise_exception(
+                GlobalException.CustomHTTPException.raise_exception(
                     500,
                     "Database error",
-                    extra={"pgerror": str(e)}
-                )
-            except ResponseValidationError as e:
-                cls.raise_exception(
-                    501,
-                    "Service Not implemented",
                     extra={"error": str(e)}
                 )
             except HTTPException as e:
-                raise e
-            except Exception as e:
-                cls.raise_exception(
-                    501,
-                    "Service Not implemented",
+                GlobalException.CustomHTTPException.raise_exception(
+                    e.status_code,
+                    e.detail,
                     extra={"error": str(e)}
                 )
+            except Exception as e:
+                GlobalException.CustomHTTPException.raise_exception(
+                    501,
+                    "Service not implemented or do not exist anymore",
+                    extra={"error": str(e)}
+                )
+
         return wrapper
 
+    class CustomHTTPException(HTTPException):
+        def __init__(
+                self,
+                status_code: int,
+                detail: str | None = None,
+                extra: dict | None = None
+        ) -> None:
+            super().__init__(status_code=status_code, detail=detail)
+            self.extra = extra
 
-async def custom_exception_handler(request: Request, exc: CustomHTTPException) -> JSONResponse | None:
+        def to_dict(self):
+            return {
+                "status_code": self.status_code,
+                "detail": self.detail,
+                "extra": self.extra,
+            }
+
+        @classmethod
+        def raise_exception(
+                cls,
+                status_code: int = 501,
+                detail: str | None = None,
+                extra: dict | None = None
+        ) -> Exception:
+            raise cls(status_code=status_code, detail=detail, extra=extra)
+
+
+async def global_exception_handler(request: Request, exc: GlobalException.CustomHTTPException) -> JSONResponse:
     return JSONResponse(status_code=exc.status_code, content=exc.to_dict())
 
 
-async def response_validation_exception_handler(request: Request, exc: ResponseValidationError):
+async def response_validation_exception_handler(request: Request, exc: ResponseValidationError) -> JSONResponse:
     return JSONResponse(
         status_code=501,
         content={
