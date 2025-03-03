1f796ab1b351947e862de578b0f68462d91aeaf2
olena4reunited <olenaduringintermission@gmail.com>
2025-03-03 14:17:11 +0200

    feat: create create_feedback controller and db model method

--- server/app/models/users_profile_feedback_model.py
+++ server/app/models/users_profile_feedback_model.py
@@ -1,5 +1,50 @@
+from typing import Any
+
 from server.app.models._base_model import BaseModel
+from server.app.database.database import PostgresDatabase
 
 
 class UserProfileFeedback(BaseModel):
     table_name = "users_profile_feedbacks"
+
+    @staticmethod
+    def create_feedback(
+            user_id: int,
+            commentator_id: int,
+            feedback: dict[str, Any]
+    ) -> dict[str, Any] | None:
+        with PostgresDatabase(on_commit=True) as db:
+            return db.fetch(
+                """
+                    WITH inserted_feedback AS (
+                        INSERT INTO users_profile_feedbacks 
+                            (content,
+                             rate,
+                             commentator_id,
+                             profile_id)
+                        VALUES (%s, %s, %s, %s)
+                        RETURNING id, content, rate, commentator_id, profile_id
+                    ),
+                    inserted_image AS (
+                        INSERT INTO profile_feedbacks_images 
+                            (image_link, profile_feedback_id)
+                        SELECT %s, inf.id
+                        FROM inserted_feedback inf
+                        WHERE %s IS NOT NULL
+                        ON CONFLICT DO NOTHING
+                        RETURNING id, image_link, profile_feedback_id
+                    )
+                    SELECT 
+                        if.id AS id,
+                        content, 
+                        rate, 
+                        commentator_id, 
+                        profile_id, 
+                        COALESCE(ini.image_link, NULL) AS image_link
+                    FROM inserted_feedback if
+                    LEFT JOIN inserted_image ini 
+                        ON ini.profile_feedback_id = if.id;
+                """,
+                (feedback["content"], feedback["rate"], commentator_id, user_id, feedback["image_link"], feedback["image_link"]),
+            )
+
