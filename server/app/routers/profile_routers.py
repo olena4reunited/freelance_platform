2ec35deb440504eca3e9199882db2a62b5f0db40
olena4reunited <olenaduringintermission@gmail.com>
2025-03-04 12:36:17 +0200

    feat/upd: move checker for feedback deletion to decorator

--- server/app/routers/profile_routers.py
+++ server/app/routers/profile_routers.py
@@ -11,11 +11,12 @@ from server.app.schemas.profile_feedback_schemas import (
 from server.app.controllers.profile_feedback_controller import ProfileFeedbackController
 from server.app.validators.profile_feedback_validators import ProfileFeedbackValidator
 from server.app.utils.exceptions import GlobalException
-from server.app.utils.dependencies import (
+from server.app.utils.dependencies.dependencies import (
     get_current_user,
     required_plans,
     required_permissions
 )
+from server.app.utils.dependencies.feedback_permissions import can_delete_feedback
 from server.app.utils.redis_client import redis_client
 
 
@@ -98,49 +99,13 @@ def update_feedback(
 @router.delete("/{user_id}/feedback/{feedback_id}", response_model=Union[list[ProfileFeedbackResponse], ProfileFeedbackResponse])
 @GlobalException.catcher
 @required_plans(["admin", "moderator", "customer", "performer"])
+@can_delete_feedback()
 def delete_feedback(
         user_id: int,
         feedback_id: int,
         user: dict[str, Any] = Depends(get_current_user),
 ):
-    try:
-        user_plan = user.get("plan")
-        user_permissions = set(json.loads(redis_client.hgetall(user_plan).get("permissions")))
-
-        if not user_plan:
-            GlobalException.CustomHTTPException.raise_exception(
-                status_code=403,
-                detail="User plan not found",
-            )
-        if user_plan in ["customer", "performer"]:
-            if not {"update_feedback_info_created_by_current_user", "delete_feedback_created_by_current_user"}.issubset(user_permissions):
-                GlobalException.CustomHTTPException.raise_exception(
-                    status_code=403,
-                    detail="User does not have permission to access resource"
-                )
-            ProfileFeedbackValidator(
-                user_id=user.get("id"),
-                feedback_id=feedback_id,
-            ). \
-            validate_feedback_commentator()
-        if user_plan in ["admin", "moderator"]:
-            if not {"read_feedbacks_details_selected_user", "delete_feedback_selected_user"}.issubset(user_permissions):
-                GlobalException.CustomHTTPException.raise_exception(
-                    status_code=403,
-                    detail="User does not have permission to access resource"
-                )
-
-        ProfileFeedbackValidator(
-            user_id=user_id,
-            feedback_id=feedback_id,
-        ). \
-        validate_feedback_user_profile()
-    except GlobalException.CustomHTTPException as e:
-        if e.status_code == 404:
-            pass
-        raise e
-    finally:
-        return ProfileFeedbackController.delete_feedback(user_id=user_id, feedback_id=feedback_id)
+    return ProfileFeedbackController.delete_feedback(user_id=user_id, feedback_id=feedback_id)
 
 
 @router.get("/{user_id}/feedback/{feedback_id}", response_model=ProfileFeedbackResponse)
--- server/app/utils/dependencies/feedback_permissions.py
+++ server/app/utils/dependencies/feedback_permissions.py
@@ -1,4 +1,5 @@
-from typing import Any
+from functools import wraps
+from typing import Any, Callable
 import json
 
 from server.app.utils.redis_client import redis_client
@@ -6,40 +7,47 @@ from server.app.utils.exceptions import GlobalException
 from server.app.validators.profile_feedback_validators import ProfileFeedbackValidator
 
 
-def can_delete_feedback(
-        user_id: int,
-        feedback_id: int,
-        user: dict[str, Any]
-):
-    user_plan = user.get("plan_name")
-    user_permissions = set(json.loads(redis_client.hgetall(user_plan).get("permissions")))
+def can_delete_feedback():
+    def decorator(func: Callable):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            user_id = kwargs.get("user_id")
+            feedback_id = kwargs.get("feedback_id")
+            user = kwargs.get("user")
 
-    if not user_plan:
-        GlobalException.CustomHTTPException.raise_exception(
-            status_code=403,
-            detail="User plan not found",
-        )
-    if user_plan in ["customer", "performer"]:
-        if not {"update_feedback_info_created_by_current_user", "delete_feedback_created_by_current_user"}.issubset(
-                user_permissions):
-            GlobalException.CustomHTTPException.raise_exception(
-                status_code=403,
-                detail="User does not have permission to access resource"
-            )
-        ProfileFeedbackValidator(
-            user_id=user.get("id"),
-            feedback_id=feedback_id,
-        ). \
-        validate_feedback_commentator()
-    if user_plan in ["admin", "moderator"]:
-        if not {"read_feedbacks_details_selected_user", "delete_feedback_selected_user"}.issubset(user_permissions):
-            GlobalException.CustomHTTPException.raise_exception(
-                status_code=403,
-                detail="User does not have permission to access resource"
-            )
+            user_plan = user.get("plan_name")
+            user_permissions = set(json.loads(redis_client.hgetall(user_plan).get("permissions")))
 
-    ProfileFeedbackValidator(
-        user_id=user_id,
-        feedback_id=feedback_id,
-    ). \
-    validate_feedback_user_profile()
+            if not user_plan:
+                GlobalException.CustomHTTPException.raise_exception(
+                    status_code=403,
+                    detail="User plan not found",
+                )
+            if user_plan in ["customer", "performer"]:
+                if not {"update_feedback_info_created_by_current_user", "delete_feedback_created_by_current_user"}.issubset(
+                        user_permissions):
+                    GlobalException.CustomHTTPException.raise_exception(
+                        status_code=403,
+                        detail="User does not have permission to access resource"
+                    )
+                ProfileFeedbackValidator(
+                    user_id=user.get("id"),
+                    feedback_id=feedback_id,
+                ). \
+                validate_feedback_commentator()
+            if user_plan in ["admin", "moderator"]:
+                if not {"read_feedbacks_details_selected_user", "delete_feedback_selected_user"}.issubset(user_permissions):
+                    GlobalException.CustomHTTPException.raise_exception(
+                        status_code=403,
+                        detail="User does not have permission to access resource"
+                    )
+
+            ProfileFeedbackValidator(
+                user_id=user_id,
+                feedback_id=feedback_id,
+            ). \
+            validate_feedback_user_profile()
+
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
