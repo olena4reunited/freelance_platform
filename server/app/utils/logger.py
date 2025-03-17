4278a2d9fda31f76ca5b8b0b7f5eec9952a36781
olena4reunited <olenaduringintermission@gmail.com>
2025-03-17 13:22:40 +0200

    feat: logger fo custom events logging

---
+++ server/app/utils/logger.py
@@ -0,0 +1,35 @@
+import logging
+
+
+class LoggerFormatter(logging.Formatter):
+    COLORS = {
+        "CRITICAL": "\x1b[1;31m",
+        "WARNING": "\x1b[1;33m",
+        "ERROR": "\x1b[31m",
+        "DEBUG": "\x1b[33m",
+        "INFO": "\x1b[32m",
+    }
+
+    reset = "\x1b[0m"
+
+    def format(self, record):
+        level_color = self.COLORS.get(record.levelname, self.reset)
+        padding = " " * (9 - len(record.levelname))
+        levelname_padded = f"{record.levelname}{self.reset}:{padding}"
+        
+        logmessage_formatted = f"{level_color}{levelname_padded}{record.msg}"
+    
+        record.msg = logmessage_formatted
+        return super().format(record)
+
+
+logger = logging.getLogger("server")
+
+logger.setLevel(logging.DEBUG)  
+
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+
+handler.setFormatter(LoggerFormatter())
+
+logger.addHandler(handler)
