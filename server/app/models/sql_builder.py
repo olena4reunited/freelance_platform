a9566c1a4a109f46e8dd744da765387f7a1f8d5c
olena4reunited <olenaduringintermission@gmail.com>
2025-03-20 14:11:13 +0200

    feat: sql builder for select queries

--- server/app/models/sql_builder.py
+++ server/app/models/sql_builder.py
@@ -1,4 +1,3 @@
-from typing import Callable
 import traceback
 
 from psycopg2 import sql
@@ -24,7 +23,65 @@ class SQLBuilder:
         self.having_colnditions = []
         self.limit = None
         self.offset = None
-        self.params = tuple()
+        self.params = []
+
+    class Group:
+        def __init__(self, parent, operator, column: str, param: str, query_operator: str = None):
+            self.parent = parent
+            self.operator = f" {operator} "
+            self.conditions = []
+            self.params = []
+
+            self._add(column=column, param=param, operator=query_operator)
+
+
+        def _add(self, column: str, param: str, operator: str = None):
+            if not operator:
+                if param is None:
+                    operator = "IS"
+                else:
+                    operator = "="
+
+            if operator.upper() not in ALLOWED_OPERATORS:
+                raise ValueError("Unsupported operator. Could not excute the query")
+
+            self.conditions.append(
+                sql.SQL("{} {} {}").format(
+                    sql.Identifier(column),
+                    sql.SQL(operator),
+                    sql.Placeholder()
+                )
+            )
+            self.params.append(param)
+
+            return self
+
+        def AND(self, column: str, param: str, operator: str = None):
+            if self.operator != " AND ":
+                raise ValueError("You are not allowed to use this operator")
+
+            return self._add(column=column, param=param, operator=operator)
+
+        def OR(self, column: str, param: str, operator: str = None):
+            if self.operator != " OR ":
+                raise ValueError("You are not allowed to use this operator")
+
+            return self._add(column=column, param=param, operator=operator)
+
+        def add_group(self, group):
+            self.conditions.append(sql.SQL("({})").format(group.comb()[0]))
+            self.params.extend(group.params)
+
+            return self
+
+        def comb(self):
+            return sql.SQL(self.operator).join(
+                condition
+                for condition in self.conditions
+            ), self.params
+        
+        def end(self):
+            return self.parent
 
     def select(self, *columns):
         if columns:
@@ -32,7 +89,7 @@ class SQLBuilder:
         if not columns:
             self.select_columns.append("*")     
         return self
-    
+
     def where(self, where_column: str, operator: str = None, **params):
         if not operator:
             if None in params.values():
@@ -45,55 +102,31 @@ class SQLBuilder:
 
         self.where_columns.append(where_column)
         self.where_operators.append(operator)
-        self.params += tuple(params.values())
-        
-        return self
-    
-    def _process_conditions(self, conditions, operator):
-        grouped_parts = []
-        
-        for condition in conditions:
-            if callable(condition):
-                temp = SQLBuilder()
-                condition(temp)
-
-                if temp.where_columns:
-                    grouped_parts.extend(
-                        sql.SQL("{} {} {}").format(
-                            sql.Identifier(temp.where_columns[i]),
-                            sql.SQL(temp.where_operators[i]),
-                            sql.Placeholder()
-                        )
-                        for i in range(len(temp.where_columns))
-                    )
-                    self.params += tuple(temp.params.values())
-            else:
-                grouped_parts.append(condition)
-
-        if grouped_parts:
-            return sql.SQL("(") + sql.SQL(f" {operator} ").join(grouped_parts) + sql.SQL(")")
-        return None
-    
-    def and_(self, *conditions):
-        if not conditions:
-            return self
-
-        grouped_parts = self._process_conditions(*conditions, "AND")
-        if grouped_parts:
-            self.grouped_parts.append(grouped_parts)
+        self.params.extend(params.values())
 
         return self
 
-    def or_(self, *conditions):
-        if not conditions:
-            return self
+    def AND(self, column, param, operator = None):
+        group = self.Group(
+            parent=self,
+            operator="AND",
+            column=column,
+            param=param,
+            query_operator=operator
+        )
+        self.grouped_parts.append(group)
+        return group
 
-        grouped_parts = self._process_conditions(*conditions, "OR")
-        if grouped_parts:
-            self.grouped_parts.append(grouped_parts)
-
-        return self
-
+    def OR(self, column, param, operator = None):
+        group = self.Group(
+            parent=self,
+            operator="OR",
+            column=column,
+            param=param,
+            query_operator=operator
+        )
+        self.grouped_parts.append(group)
+        return group
 
     def get(self):
         if self.select_columns:
@@ -104,6 +137,7 @@ class SQLBuilder:
                 ),
                 sql.Identifier(self.table_name)
             )
+            
         if self.where_columns or self.grouped_parts:
             query += sql.SQL(" WHERE ")
 
@@ -116,48 +150,41 @@ class SQLBuilder:
                     )
                     for i in range(len(self.where_columns))
                 )
+
             if self.grouped_parts:
-                query += sql.SQL(" AND ").join(self.grouped_parts)
+                group_queries = []
+                group_params = []
+
+                for group in self.grouped_parts:
+                    query_part, params = group.comb()
+                    group_queries.append(query_part)
+                    group_params.extend(params)
+
+                query += sql.SQL(" AND ").join(group_queries)
+                self.params.extend(group_params)
 
         return query, self.params
-   
 
-try:
-    with PostgresDatabase() as db:
-        with db.connection.cursor() as cursor:        
-            query, params = (
-                SQLBuilder("users") \
-                .select("username") \
-                .and_(
-                    lambda q: q.where("is_blocked", is_blocked=None),
-                    lambda q: q.or_(
-                        lambda q: q.where("id", id=20),
-                        lambda q: q.where("id", id=2)
-                    )
-                ) \
-                .get()
-            )
-            query, params = (
-                SQLBuilder("users") \
-                .select("username") \
-                .or_(
-                    lambda q: q.where("id", id=20),
-                    lambda q: q.where("id", id=2)
-                ) \
-                .get()
-            )
-            # query, params = SQLBuilder("users").select().where("username", "=", username='reptilia"; DROP TABLE users;').get()
-            # query, params = SQLBuilder("users").select().where("username", "=", username="' OR 1=1 --").get()
 
-            cursor.execute(query, params)
+# try:
+#     with PostgresDatabase() as db:
+#         with db.connection.cursor() as cursor:
+#             # sql_builder = SQLBuilder("users").select("username").AND(column="is_blocked", param=None).add_group(SQLBuilder().OR(column="id", param="2").OR(column="id", param="20")).end()
+#             # print(sql_builder) 
 
-            logger.info("\x1b[1m%s\x1b[0m", query.as_string(cursor))
-            logger.info("\x1b[1m%s\x1b[0m", params)
-            logger.info("\x1b[1;32m%s\x1b[0m", cursor.fetchall())
-except Exception as e:
-    traceback.format_exception(e)
-    logger.error(
-        "Error was occured:\n%s\x1b[31m%s\x1b[0m",
-        " "*10,
-        repr(e)
-    )
+#             query, params = SQLBuilder("payments").select("id", "payment").where("user_id", params=4).get()
+
+
+#             logger.info("\x1b[1m%s\x1b[0m", query.as_string(cursor))
+
+#             cursor.execute(query, params)
+
+#             logger.info("\x1b[1m%s\x1b[0m", params)
+#             logger.info("\x1b[1;32m%s\x1b[0m", cursor.fetchall())
+# except Exception as e:
+#     traceback.print_exc()
+#     logger.error(
+#         "Error was occured:\n%s\x1b[31m%s\x1b[0m",
+#         " "*10,
+#         repr(e)
+#     )
