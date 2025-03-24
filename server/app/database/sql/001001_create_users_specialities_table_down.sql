21a6f059229da4f030585f5bcdef8e66c01e4566
olena4reunited <olenaduringintermission@gmail.com>
2025-03-24 16:59:35 +0200

    feat: create specialities for performers and tags for orders and performers, implement teams of performers

---
+++ server/app/database/sql/001001_create_users_specialities_table_down.sql
@@ -0,0 +1,11 @@
+BEGIN;
+
+DROP TABLE IF EXISTS teams_users;
+DROP TABLE IF EXISTS teams;
+DROP TABLE IF EXISTS users_specialities;
+DROP TABLE IF EXISTS orders_tags;
+DROP TABLE IF EXISTS specialities_tags;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS specialities;
+
+COMMIT;
---
+++ server/app/database/sql/001001_create_users_specialities_table_up.sql
@@ -0,0 +1,61 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS specialities (
+    id SERIAL PRIMARY KEY,
+    speciality VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tags (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS specialities_tags (
+    id SERIAL PRIMARY KEY,
+    speciality_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+    FOREIGN KEY (speciality_id) REFERENCES specialities(id) ON DELETE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE,
+    UNIQUE (speciality_id, tag_id)
+);
+
+CREATE TABLE IF NOT EXISTS orders_tags (
+    id SERIAL PRIMARY KEY,
+    order_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE,
+    UNIQUE (order_id, tag_id)
+);
+
+CREATE TABLE IF NOT EXISTS users_specialities (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    speciality_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (speciality_id) REFERENCES specialities(id) ON DELETE CASCADE,
+    UNIQUE (user_id, speciality_id)
+);
+
+CREATE TABLE IF NOT EXISTS teams (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    lead_id INTEGER NOT NULL,
+    customer_id INTEGER NOT NULL,
+    FOREIGN KEY (lead_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (customer_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS teams_users (
+    id SERIAL PRIMARY KEY,
+    team_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    UNIQUE (team_id, user_id)
+);
+
+ALTER TABLE orders ADD COLUMN performer_team_id INTEGER NULL;
+ALTER TABLE orders ADD CONSTRAINT fk_performer_team FOREIGN KEY (performer_team_id) REFERENCES teams(id) ON DELETE SET NULL;
+
+COMMIT;
